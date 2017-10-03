3d44985ecd82b1e4879a018978b0ef3ebbc79ef3
shogo_mase <mase@level-five.jp>
2017-10-03 17:28:11 +0900

    supported python 3.6

---
+++ keras_vae.py
@@ -0,0 +1,171 @@
+'''This script demonstrates how to build a variational autoencoder with Keras.
+
+Reference: "Auto-Encoding Variational Bayes" https://arxiv.org/abs/1312.6114
+'''
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from keras.layers import Input, Dense, Lambda, Layer, Activation
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+from keras import backend as K
+from keras import metrics, optimizers
+from keras.callbacks import Callback
+import keras
+
+
+print(keras.__version__)
+tf.__version__
+
+def sampling(args):
+    z_mean, z_log_var = args
+    epsilon = K.random_normal(shape=(K.shape(z_mean)[0], latent_dim), mean=0.,
+                              stddev=epsilon_std)
+    return z_mean + K.exp(z_log_var / 2) * epsilon
+
+# Custom loss layer
+class CustomVariationalLayer(Layer):
+    def __init__(self, **kwargs):
+        self.is_placeholder = True
+        super(CustomVariationalLayer, self).__init__(**kwargs)
+
+    def vae_loss(self, x, x_decoded_mean):
+        xent_loss = original_dim * metrics.binary_crossentropy(x, x_decoded_mean)
+        kl_loss = - 0.5 * K.sum(1 + z_log_var_encoded - K.square(z_mean_encoded) -
+                                K.exp(z_log_var_encoded), axis=-1)
+        return K.mean(xent_loss + (K.get_value(beta) * kl_loss))
+
+    def call(self, inputs):
+        x = inputs[0]
+        x_decoded_mean = inputs[1]
+        loss = self.vae_loss(x, x_decoded_mean)
+        self.add_loss(loss, inputs=inputs)
+        # We won't actually use the output.
+        return x
+
+class WarmUpCallback(Callback):
+    def __init__(self, beta, kappa):
+        self.beta = beta
+        self.kappa = kappa
+
+    # Behavior on each epoch
+    def on_epoch_end(self, epoch, logs={}):
+        if K.get_value(self.beta) <= 1:
+            K.set_value(self.beta, K.get_value(self.beta) + self.kappa)
+
+
+np.random.seed(123)
+rnaseq_file = os.path.join('data', 'pancan_scaled_zeroone_rnaseq.tsv')
+rnaseq_df = pd.read_table(rnaseq_file, index_col=0)
+print(rnaseq_df.shape)
+
+test_set_percent = 0.1
+rnaseq_test_df = rnaseq_df.sample(frac=test_set_percent)
+rnaseq_train_df = rnaseq_df.drop(rnaseq_test_df.index)
+
+original_dim = rnaseq_df.shape[1]
+latent_dim = 100
+
+batch_size = 50
+epochs = 50
+learning_rate = 0.0005
+
+epsilon_std = 1.0
+beta = K.variable(0)
+kappa = 1
+
+# ## Load Gene Expression Data
+
+# ## Encoder
+
+rnaseq_input = Input(shape=(original_dim,))
+z_mean_dense_linear = Dense(latent_dim, kernel_initializer='glorot_uniform')(rnaseq_input)
+z_mean_dense_batchnorm = BatchNormalization()(z_mean_dense_linear)
+z_mean_encoded = Activation('relu')(z_mean_dense_batchnorm)
+
+z_log_var_dense_linear = Dense(latent_dim, kernel_initializer='glorot_uniform')(rnaseq_input)
+z_log_var_dense_batchnorm = BatchNormalization()(z_log_var_dense_linear)
+z_log_var_encoded = Activation('relu')(z_log_var_dense_batchnorm)
+z = Lambda(sampling, output_shape=(latent_dim,))([z_mean_encoded, z_log_var_encoded])
+
+# ## Decoder
+decoder_to_reconstruct = Dense(original_dim, kernel_initializer='glorot_uniform', activation='sigmoid')
+rnaseq_reconstruct = decoder_to_reconstruct(z)
+
+adam = optimizers.Adam(lr=learning_rate)
+vae_layer = CustomVariationalLayer()([rnaseq_input, rnaseq_reconstruct])
+vae = Model(rnaseq_input, vae_layer)
+vae.compile(optimizer=adam, loss=None, loss_weights=[beta])
+
+vae.summary()
+
+hist = vae.fit(np.array(rnaseq_train_df),
+               shuffle=True,
+               epochs=epochs,
+               batch_size=batch_size,
+               validation_data=(np.array(rnaseq_test_df), None),
+               callbacks=[WarmUpCallback(beta, kappa)])
+
+
+
+encoder = Model(rnaseq_input, z_mean_encoded)
+encoded_rnaseq_df = encoder.predict_on_batch(rnaseq_df)
+encoded_rnaseq_df = pd.DataFrame(encoded_rnaseq_df, index=rnaseq_df.index)
+
+encoded_rnaseq_df.columns.name = 'sample_id'
+encoded_rnaseq_df.columns = encoded_rnaseq_df.columns + 1
+encoded_file = os.path.join('data', 'encoded_rnaseq_onehidden_warmup_batchnorm.tsv')
+encoded_rnaseq_df.to_csv(encoded_file, sep='\t')
+
+decoder_input = Input(shape=(latent_dim,))  # can generate from any sampled z vector
+_x_decoded_mean = decoder_to_reconstruct(decoder_input)
+decoder = Model(decoder_input, _x_decoded_mean)
+encoder_model_file = os.path.join('models', 'encoder_onehidden_vae.hdf5')
+decoder_model_file = os.path.join('models', 'decoder_onehidden_vae.hdf5')
+
+encoder.save(encoder_model_file)
+decoder.save(decoder_model_file)
+
+sum_node_activity = encoded_rnaseq_df.sum(axis=0).sort_values(ascending=False)
+
+# Top 10 most active nodes
+print(sum_node_activity.head(10))
+
+# Bottom 10 least active nodes
+sum_node_activity.tail(10)
+
+sum_node_activity.hist()
+plt.xlabel('Activation Sum')
+plt.ylabel('Count');
+
+# What does an example distribution of two latent features look like?
+
+# In[23]:
+
+# Example of node activation distribution for the first two latent features
+plt.figure(figsize=(6, 6))
+plt.scatter(encoded_rnaseq_df.iloc[:, 1], encoded_rnaseq_df.iloc[:, 2])
+plt.xlabel('Latent Feature 1')
+plt.xlabel('Latent Feature 2');
+
+# ###  Observe reconstruction fidelity
+
+# In[24]:
+
+# How well does the model reconstruct the input RNAseq data
+input_rnaseq_reconstruct = decoder.predict(np.array(encoded_rnaseq_df))
+input_rnaseq_reconstruct = pd.DataFrame(input_rnaseq_reconstruct, index=rnaseq_df.index,
+                                        columns=rnaseq_df.columns)
+input_rnaseq_reconstruct.head(2)
+
+# In[25]:
+
+reconstruction_fidelity = rnaseq_df - input_rnaseq_reconstruct
+
+gene_mean = reconstruction_fidelity.mean(axis=0)
+gene_abssum = reconstruction_fidelity.abs().sum(axis=0).divide(rnaseq_df.shape[0])
+gene_summary = pd.DataFrame([gene_mean, gene_abssum], index=['gene mean', 'gene abs(sum)']).T
+gene_summary.sort_values(by='gene abs(sum)', ascending=False).head()
